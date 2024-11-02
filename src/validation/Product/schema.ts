90fa0d37661b8e2bf0e22f5676a2c4bededeb489
NoufalTrianto <noufaltrianto@gmail.com>
2024-11-02 20:12:27 +0700

    mengubah joi description jadi optional

--- src/validation/Product/schema.ts
+++ src/validation/Product/schema.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const createProductSchema = Joi.object({
     name: Joi.string().required(),
-    description: Joi.string().required(),
+    description: Joi.string().optional(),
     price: Joi.number().required(),
     stock: Joi.number().required(),
    category_id: Joi.number().required(),
@@ -19,7 +19,7 @@ const getAllProductSchema = Joi.object({
 const  updateProductSchema = Joi.object({
     id: Joi.number().required(),
     name: Joi.string().required(),
-    description: Joi.string().required(),
+    description: Joi.string().optional(),
     price: Joi.number().required(),
     stock: Joi.number().required(),
     category_id: Joi.number().required(),
