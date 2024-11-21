fa503d4cc4783b1c2d79a1696c5943f251b7a4e1
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-11-21 22:38:33 +0700

    feat: Added validation to createCheckout function

--- src/api/controllers/CheckOut/CreateCheckout.ts
+++ src/api/controllers/CheckOut/CreateCheckout.ts
@@ -4,6 +4,11 @@ import { CheckoutStatus } from "@prisma/client";
 import { createCheckoutSchema } from "../../../validation/CheckOut/schema";
 import CheckoutService from "../../../libs/services/CheckOut";
 import InvariantError from "../../../utils/exceptions/InvariantError";
+import userService from "../../../libs/services/Users";
+import cartService from "../../../libs/services/Cart";
+import shippingService from "../../../libs/services/Shipping";
+import addressService from "../../../libs/services/Address";
+// import paymentService from "../../../libs/services/Payment";
 
 interface CreateCheckoutRequest extends Request {
   body: {
@@ -26,6 +31,31 @@ export const createCheckout = async (
   const { error } = createCheckoutSchema.validate(req.body);
   if (error) {
     throw new InvariantError(error.details[0].message);
+  }
+
+  const checkUserId = await userService.getUserById(user_id);
+  if (!checkUserId) {
+    throw new InvariantError("User ID not found!");
+  }
+
+  const checkCartId = await cartService.getCartById(cart_id);
+  if (!checkCartId) {
+    throw new InvariantError("Cart ID not found!");
+  }
+
+  // const checkPaymentId = await paymentService.getPaymentById(payment_id);
+  // if (!checkPaymentId) {
+  //   throw new InvariantError("Payment ID not found!");
+  // }
+
+  const checkShippingId = await shippingService.getShippingById(shipping_id);
+  if (!checkShippingId) {
+    throw new InvariantError("Shipping ID not found!");
+  }
+
+  const checkAddressId = await addressService.getAddressById(address_id);
+  if (!checkAddressId) {
+    throw new InvariantError("Address ID not found!");
   }
 
   const checkout = await CheckoutService.createCheckout({
